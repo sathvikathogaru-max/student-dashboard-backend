5aee4b2da6905766e2e17eaf245f8f541ae0d570
Sathvikathogaru <sathvikathogaru@gmail.com>
2026-04-09 14:56:14 +0530

    fixed render port

--- server.js
+++ server.js
@@ -99,6 +99,14 @@ app.get("/", (req, res) => {
   res.send("Student Portal Backend Running 🚀");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
+
+
+
+
+
